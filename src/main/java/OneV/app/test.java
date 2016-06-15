b5dd299d6f1d2098e44489d9e1e6a2f4ba8e92db
kkuznetsov <theunusingname@gmail.com>
2016-06-15 16:09:13 +0300

    stage1

--- src/main/java/OneV/app/test.java
+++ src/main/java/OneV/app/test.java
@@ -12,22 +12,21 @@ import java.util.Random;
  */
 public class test {
     public static void main(String[] args) {
-
+        System.out.println((double) 99/10);
         FileOutputStream fos;
         File outFile;
-        int aggregatedIntAAA=0;
-        float aggregatedFloatAAA=0;
-        int aggregatedIntBBB=0;
-        float aggregatedFloatBBB=0;
-        float aggregatedFloatAAAstreamInt[]={0,0,0};
-        float aggregatedFloatBBBstreamInt[]={0,0,0};
-        float aggregatedFloatAAAstreamVar[]={0,0,0};
-        float aggregatedFloatBBBstreamVar[]={0,0,0};
-        float aggregatedFloatAAAstreamFloat[]={0,0,0};
-        float aggregatedFloatBBBstreamFloat[]={0,0,0};
-        float aggregatedFloatAAAstreamDate[]={0,0,0};
-        float aggregatedFloatBBBstreamDate[]={0,0,0};
+        int aggregatedGlobalInt=0;
+        float aggregatedGlobalFloat=0;
+        int aggregatedInt[]={0,0,0};
+        double aggregatedFloat[]={0,0,0};
+
+        double aggregatedFloatStreamInt[][]={{0,0,0},{0,0,0},{0,0,0}};
+        double aggregatedFloatStreamVar[][]={{0,0,0},{0,0,0},{0,0,0}};
+        double aggregatedFloatStreamFloat[][]={{0,0,0},{0,0,0},{0,0,0}};
+        double aggregatedFloatStreamDate[][]={{0,0,0},{0,0,0},{0,0,0}};
+
         String varcharVariants[]={"aaa","bbb","ccc"};
+        int streamIntVariants[]={1,2,3};
         String streamVarchVariants[]={"a","b","c"};
         String streamFloatVariants[]={"0.0","0.1","0.2"};
         Date streamDateVariantes[]={new Date(86400000),new Date(172800000),new Date(345600000)};
@@ -51,32 +50,75 @@ public class test {
             }
 
             Random rnd=new Random(hash);
-            for(int i=0;i<20;i++)
+            for(int i=0;i<200;i++)
             {
                 if (i!=0) fos.write("\n".getBytes());
                 String nextVarchVal=varcharVariants[rnd.nextInt(3)];
-                int nextIntVal=rnd.nextInt(1000);
-                Float nextFloat =rnd.nextFloat();
-                String nextFloatVal= nextFloat.toString();
-                nextFloatVal= nextFloatVal.substring(0,4);
-                int nextStreamInt =rnd.nextInt(3)+1;
+                int nextIntVal=i;
+                double nextFloat = (double) rnd.nextInt(100)/10;
+                int nextStreamInt =streamIntVariants[rnd.nextInt(3)];
                 String nextStreamVarch=streamVarchVariants[rnd.nextInt(3)];
                 String nextStreamFloat=streamFloatVariants[rnd.nextInt(3)];
                 Date nextStreamDate=streamDateVariantes[rnd.nextInt(3)];
 
-                fos.write((nextVarchVal+"\t"+nextIntVal+"\t"+nextFloatVal+"\t"+nextStreamInt+"\t"
-                        +nextStreamVarch+"\t"+nextStreamFloat +"\t"+nextStreamDate.toLocaleString()).getBytes());
+                fos.write((nextVarchVal+"\t"+nextIntVal+"\t"+nextFloat+"\t"+nextStreamInt+"\t"
+                        +nextStreamVarch+"\t"+nextStreamFloat +"\t"+nextStreamDate.toLocaleString().substring(0,10)).getBytes());
 
-                aggregatedFloatAAA+=new Float(nextFloatVal);
-                aggregatedIntAAA+=nextIntVal;
+                aggregatedGlobalFloat+=nextFloat;
+                aggregatedGlobalInt+=nextIntVal;
 
-                if(nextVarchVal=="aaa"){}
+                for (int j=0;j<3;j++) {                                                             //j-details k-stream
+                    if (nextVarchVal == varcharVariants[j]) {
+                        aggregatedFloat[j] += nextFloat;
+                        for (int k = 0; k < 3; k++) {
+                            if (nextStreamDate == streamDateVariantes[k])
+                                aggregatedFloatStreamDate[k][j] += nextFloat;
 
+                            if (nextStreamFloat == streamFloatVariants[k])
+                                aggregatedFloatStreamFloat[k][j] += nextFloat;
 
+                            if (nextStreamInt == streamIntVariants[k])
+                                aggregatedFloatStreamInt[k][j] += nextFloat;
+
+                            if (nextStreamVarch == streamVarchVariants[k])
+                                aggregatedFloatStreamVar[k][j] += nextFloat;
+                        }
+                    }
+                }
 
             }
 
+            fos.close();
+            File resultByDetails=new File("ResultByDetails");
+            FileOutputStream resiltByDetailsOutStr=new FileOutputStream(resultByDetails);
 
+            for(int k=0;k<3;k++)
+            {
+                resiltByDetailsOutStr.write((varcharVariants[k]+"\t"+new Double( aggregatedFloat[k]).floatValue()+"\n").getBytes());
+
+                File resultVarchStream=new File("resultVarchStream"+"_"+k);
+                FileOutputStream resultByVarchOut=new FileOutputStream(resultVarchStream);
+                File resultDateStream=new File("resultDateStream"+"_"+k);
+                FileOutputStream resultByDateOut=new FileOutputStream(resultDateStream);
+                File resultFloatStream=new File("resultFloatStream"+"_"+k);
+                FileOutputStream resultByFloatOut=new FileOutputStream(resultFloatStream);
+                File resultIntStream=new File("resultIntStream"+"_"+k);
+                FileOutputStream resultByIntOut=new FileOutputStream(resultIntStream);
+
+                for(int j=0;j<3;j++)
+                {
+                    resultByVarchOut.write((varcharVariants[j]+"\t"+new Double( aggregatedFloatStreamVar[k][j]).floatValue()+"\n").getBytes());
+                    resultByDateOut.write((varcharVariants[j]+"\t"+ new Double( aggregatedFloatStreamDate[k][j]).floatValue()+"\n").getBytes());
+                    resultByFloatOut.write((varcharVariants[j]+"\t"+ new Double( aggregatedFloatStreamFloat[k][j]).floatValue()+"\n").getBytes());
+                    resultByIntOut.write((varcharVariants[j]+"\t"+new Double( aggregatedFloatStreamInt[k][j]).floatValue()+"\n").getBytes());
+                }
+                resultByDateOut.close();
+                resultByFloatOut.close();
+                resultByIntOut.close();
+                resultByVarchOut.close();
+
+            }
+            resiltByDetailsOutStr.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
